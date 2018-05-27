dedb3dccf268fbff3d1d69dd8aea9347740844f3
Ujjwal Sharma <usharma1998@gmail.com>
2018-05-27 19:19:41 +0530

    Improve server

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
-const app = require('express')();
+const express = require('express');
+
+const app = express();
+
+app.use(express.json());
 
 app.post('/github', (req, res) => {
-  console.log(req);
-  res.sendStatus(200);
+  const event = req.headers['x-github-event'];
+  if (!event) {
+    return res.sendStatus(400);
+  }
+  console.log(event, req.body.action, req.body.member);
+  return res.sendStatus(200);
 });
 
 app.listen(1337, () => console.log('Listening for webhooks on port 1337!'));
